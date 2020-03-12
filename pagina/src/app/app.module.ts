be32fbc56025c80b170a26299973e7769d2afa21
fabinajm <fabio.abinajm@gmail.com>
2020-03-12 14:33:33 -0300

    Flex layout

--- pagina/src/app/app.module.ts
+++ pagina/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule} from '@angular/material'; 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { AppRoutingModule, routingComponents } from './app-routing.module';
